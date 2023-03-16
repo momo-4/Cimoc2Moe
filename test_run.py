6d6fe54086ceb8d929188694a1d32f9fe86a2210
momo <momo_@toke.com>
2023-03-16 22:30:55 +0800

    refresh test_run.py

--- test_run.py
+++ test_run.py
@@ -1,9 +1,9 @@
 import cimoc2moe
 
 # 从Cimoc获得的漫画文件夹
-d = 'F:\\file'
+dir = 'F:\\file'
 
-a = cimoc2moe.Cimoc2Mox(source=d)
-a.read()
-a.copy()
-a.zipping()
+c2m = cimoc2moe.Cimoc2Mox(source=dir)
+c2m.read()
+c2m.copy()
+c2m.zipping()
